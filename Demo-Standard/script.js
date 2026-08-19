b81ff602dbf223ee310cf5f19d2919e4631f4512
Ahmad Amsari <35188610+kienon@users.noreply.github.com>
2026-08-19 16:30:47 +0800

    Delete Demo-Standard/script.js

--- Demo-Standard/script.js
+++
@@ -1,8 +0,0 @@
-// Fungsi letupkan konfeti untuk meraikan hari lahir
-function triggerConfetti() {
-    confetti({
-        particleCount: 110,
-        spread: 75,
-        origin: { y: 0.6 }
-    });
-}
